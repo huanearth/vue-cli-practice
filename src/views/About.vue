7590d1a8d142a7fd4e2f3f01f340c799e9843951
zhangh04 <1336579845@qq.com>
2021-05-10 19:13:50 +0800

    引入UI组件，相应环境配置

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>zhanghuan page</h1>
+    <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
   </div>
 </template>
